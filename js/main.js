03388f1c41f772b173b02cfbd99a0d71db8aa53e
Gabriel Claudino <gabrclaudino@gmail.com>
2021-07-12 18:30:26 -0300

    new files

---
+++ js/main.js
@@ -0,0 +1,6 @@
+/*==== MENU SHOW & HIDDEN ====*/
+const navMenu = document.getElementById('nav-menu'),
+    toggleMenu = document.getElementById('nav-toggle'),
+    closeMenu = document.getElementById('nav-close')
+
+    
